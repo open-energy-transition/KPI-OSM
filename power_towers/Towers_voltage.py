e401c6f899b1953de00b77b8367ba05b2d5b1e41
Andreas Hernandez Denyer <107207227+AndreasHD11@users.noreply.github.com>
2025-03-31 12:20:30 +0200

    Update Towers_voltage.py

--- power_towers/Towers_voltage.py
+++ power_towers/Towers_voltage.py
@@ -94,7 +94,7 @@ def display_results(tower_voltages):
 
 # ---------------------- Main Execution ---------------------- #
 if __name__ == "__main__":
-    country = input(f"Enter the country name (default: {DEFAULT_COUNTRY}): ") or DEFAULT_COUNTRY
+    country = input(f"Enter the country name (Example: {DEFAULT_COUNTRY}): ") or DEFAULT_COUNTRY
     users = input("Enter OSM usernames separated by commas: ").strip()
     users = [user.strip() for user in users.split(",")] if users else []
     
